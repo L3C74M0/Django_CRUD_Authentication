612e0ed73551a19464a701ed25ddcbf54586af4e
David Lectamo <47828446+L3C74M0@users.noreply.github.com>
2023-05-30 11:45:27 -0500

    Added docker to this project

--- main/settings.py
+++ main/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!w_r@wxroeu23$nm%hk)xj30l#%v^38zn#^ws$u*z31q4x)ch5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
